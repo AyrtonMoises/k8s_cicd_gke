77e24601e5cfe0b72dad49ae1d78fcd86f7940f8
AyrtonMoises <ayrton.sousa@gmail.com>
2023-01-04 16:50:04 -0300

    Alterações no scripts.sh e de cd

--- k8s/scripts.sh
+++ k8s/scripts.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 kubectl apply -f secrets.yaml
 
 kubectl apply -f configmaps.yaml
@@ -10,6 +9,10 @@ kubectl apply -f purple-deploy.yaml
 
 kubectl apply -f blue-deploy.yaml
 
-kubectl apply -f ingress.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/cloud/deploy.yaml
+
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.0/cert-manager.yaml
 
 kubectl apply -f clusterissuer.yaml
+
+kubectl apply -f ingress.yaml
